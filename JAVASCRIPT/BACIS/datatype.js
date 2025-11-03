88f67ff7b0c2d9026cfcbc09918175dddf112e87
Subham Narayan <subhamnarayan11144@gmail.com>
2025-11-03 23:46:06 +0530

    update

--- JAVASCRIPT/BACIS/datatype.js
+++
@@ -1,9 +0,0 @@
-typeof 10;          // "number"
-typeof "Hello";     // "string"
-typeof true;        // "boolean"
-typeof undefined;   // "undefined"
-typeof null;        // "object"   <-- special case (historical bug)
-typeof Symbol();    // "symbol"
-typeof [1,2,3];     // "object"
-typeof {a:1};       // "object"
-typeof function(){} // "function"
