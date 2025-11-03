b842a92456f7763bca9256f70517bac48ab938e0
Shubham Naryan <shubhamnarayan11144@gmail.com>
2025-11-03 09:31:51 +0530

    Add files via upload

---
+++ JAVASCRIPT/BACIS/date.js
@@ -0,0 +1,14 @@
+// let date=new Date()
+
+// console.log(date.toString())
+// console.log(date.toDateString())
+// console.log(typeof date)
+
+// let mydate = new Date(2023,0,19) // month start with 0
+
+//  let mydate = new Date(2023,0,19,5,6) 
+
+let mydate = new Date("01-14-2025") 
+console.log(mydate.toLocaleString())
+
+
